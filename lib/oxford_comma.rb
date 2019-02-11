178ab3da0357a1cf32704f2603d607d7a7940f24
Laura Hoffman <lhoffmanwg@gmail.com>
2019-02-11 00:29:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2 
     array.join(" and ")
   #elsif array.length == 3
-   # array.join(,)
+    array.join(",")
+    array.push("and")
     
     
   end
